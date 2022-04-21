61eff0e460b9930fda800645dda6f199dec7e311
Carlos Pavajeau <cantte098@gmail.com>
2022-04-21 11:24:01 -0500

    feat: add secured annotation in controller

--- src/main/kotlin/com/dew/InventoryController.kt
+++ src/main/kotlin/com/dew/InventoryController.kt
@@ -8,10 +8,13 @@ import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
+import io.micronaut.security.annotation.Secured
+import io.micronaut.security.rules.SecurityRule
 import reactor.core.publisher.Mono
 import javax.validation.Valid
 
 @Controller("/inventory")
+@Secured(SecurityRule.IS_AUTHENTICATED)
 open class InventoryController(
     private val inventoryService: InventoryService
 ) {
