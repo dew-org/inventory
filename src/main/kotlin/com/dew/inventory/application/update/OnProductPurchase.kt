c398f5f435320a9d3d93dc088b38a9b3d4323b6b
Carlos Pavajeau <cantte098@gmail.com>
2022-10-19 19:07:30 -0500

    fix: use '@Subscription' pub/sub annotation

--- src/main/kotlin/com/dew/inventory/application/update/OnProductPurchase.kt
+++ src/main/kotlin/com/dew/inventory/application/update/OnProductPurchase.kt
@@ -2,10 +2,10 @@ package com.dew.inventory.application.update
 
 import com.dew.common.domain.invoices.PurchasedProduct
 import com.dew.inventory.application.InventoryService
-import io.micronaut.configuration.kafka.annotation.Topic
 import io.micronaut.context.annotation.Requires
 import io.micronaut.context.env.Environment
 import io.micronaut.gcp.pubsub.annotation.PubSubListener
+import io.micronaut.gcp.pubsub.annotation.Subscription
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import reactor.core.publisher.Mono
@@ -16,7 +16,7 @@ class OnProductPurchase(private val inventoryService: InventoryService) {
 
     private val logger: Logger = LoggerFactory.getLogger(OnProductPurchase::class.java)
 
-    @Topic("product-purchase")
+    @Subscription("product-purchase")
     fun productPurchase(product: Mono<PurchasedProduct>): Mono<PurchasedProduct> {
         return product.flatMap {
             inventoryService.decreaseStock(it.code, it.quantity).flatMap { updated: Boolean ->
